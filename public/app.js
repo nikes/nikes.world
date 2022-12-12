d16d5f8fe369861c8fdbb70036f052d1d6f80b9e
Nikes <balashovalex858@gmail.com>
2022-12-12 22:52:20 +0200

    update: public page

--- public/app.js
+++ public/app.js
@@ -1,10 +1,42 @@
 (function () {
+  let elementStatus = document.getElementById('energy-status');
+  let elementLastUpdate = document.getElementById('energy-last-update');
+  elementStatus.innerText = 'Connecting...';
+
   let ws;
+
+  function setState(state) {
+    elementStatus.classList.remove(...elementStatus.classList);
+    if (state === 'offline') {
+      elementStatus.innerText = 'Offline';
+      elementStatus.classList.add('offline');
+    }
+    if (state === 'online') {
+      elementStatus.innerText = 'Online';
+      elementStatus.classList.add('online');
+    }
+    if (state === 'ups') {
+      elementStatus.innerText = 'Reserve Power';
+      elementStatus.classList.add('ups');
+    }
+  }
 
   function connect() {
     ws = new WebSocket(`wss://${location.host}`);
     ws.onmessage = (event) => {
-      console.log(`[WS][Data]`, event.data);
+      const data = JSON.parse(event.data);
+      if ((Date.now() - data.last) < 60_000 && data.data) {
+        if (data.data.power_supply) {
+          setState('online');
+        } else {
+          setState('ups');
+        }
+      } else {
+        setState('offline');
+      }
+      if (data.last > -1) {
+        elementLastUpdate.innerText = new Date(data.last).toLocaleString();
+      }
     };
     ws.onclose = () => {
       setTimeout(() => this.connect(), 1000);
