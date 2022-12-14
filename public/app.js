911bc81ffae117bd5335ea6822adb35f297f52ad
Nikes <balashovalex858@gmail.com>
2022-12-14 11:11:44 +0200

    update: public page

--- public/app.js
+++ public/app.js
@@ -1,40 +1,56 @@
 (function () {
-  let elementStatus = document.getElementById('energy-status');
+  let elementStatus = document.getElementById('energy-status-wrap');
+  let elementStatusIcon = document.getElementById('energy-status-icon');
+  let elementStatusText = document.getElementById('energy-status');
   let elementLastUpdate = document.getElementById('energy-last-update');
-  elementStatus.innerText = 'Connecting...';
 
   let ws;
-  let currentStatus;
+  let currentStatus = 'offline';
 
   function setState(state) {
     if (currentStatus === state) {
       return;
     }
+    let lastState = currentStatus;
     currentStatus = state;
-    elementStatus.classList.remove(...elementStatus.classList);
+    if (state === 'connecting') {
+      elementStatusText.innerText = 'Connecting...';
+      elementStatusIcon.hidden = true;
+      elementStatus.classList.replace(`energy-status--${lastState}`, `energy-status--${state}`);
+      elementStatus.classList.replace(`shimmer--${lastState}`, `shimmer--${state}`);
+    }
     if (state === 'offline') {
-      elementStatus.innerText = 'Offline';
-      elementStatus.classList.add('offline');
+      elementStatusText.innerText = 'Offline';
+      elementStatusIcon.src = 'images/status/offline.svg';
+      elementStatusIcon.hidden = false;
+      elementStatus.classList.replace(`energy-status--${lastState}`, `energy-status--${state}`);
+      elementStatus.classList.replace(`shimmer--${lastState}`, `shimmer--${state}`);
     }
     if (state === 'online') {
-      elementStatus.innerText = 'Online';
-      elementStatus.classList.add('online');
+      elementStatusText.innerText = 'Online';
+      elementStatusIcon.src = 'images/status/online.svg';
+      elementStatusIcon.hidden = false;
+      elementStatus.classList.replace(`energy-status--${lastState}`, `energy-status--${state}`);
+      elementStatus.classList.replace(`shimmer--${lastState}`, `shimmer--${state}`);
     }
-    if (state === 'ups') {
-      elementStatus.innerText = 'Reserve Power';
-      elementStatus.classList.add('ups');
+    if (state === 'reserve') {
+      elementStatusText.innerText = 'Reserve Power';
+      elementStatusIcon.src = 'images/status/reserve.svg';
+      elementStatusIcon.hidden = false;
+      elementStatus.classList.replace(`energy-status--${lastState}`, `energy-status--${state}`);
+      elementStatus.classList.replace(`shimmer--${lastState}`, `shimmer--${state}`);
     }
   }
 
   function connect() {
-    ws = new WebSocket(`wss://${location.host}`);
+    ws = new WebSocket(`wss://nikes.world`);
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
       if ((Date.now() - data.last) < 60_000 && data.data) {
         if (data.data.power_supply) {
           setState('online');
         } else {
-          setState('ups');
+          setState('reserve');
         }
       } else {
         setState('offline');
